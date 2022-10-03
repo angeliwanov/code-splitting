ae321938b6a5e9688fbffc31c5359af23c91c8b7
angeliwanov <angel.iwanov@gmail.com>
2022-10-03 15:13:25 +0300

    react.lazy() + higher order async component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, {useState, Suspense} from 'react';
 import './App.css';
+import AsyncComponent from './components/AsyncComponent';
+import Page1 from './components/Page1';
+ 
+//page2 is imported with react.lazy() and suspense
+//page3 is imported by creating a higher order async component
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [route, setRoute] = useState('page1');
+
+  const onRouteChange = (value) => {
+    setRoute(value)
+  }
+
+  switch (route) {
+    case 'page1':
+      return <Page1 onRouteChange={onRouteChange}/>
+    case 'page2':
+      const Page2 = React.lazy(() => import ('./components/Page2'));
+      return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <Page2 onRouteChange={onRouteChange}/>
+      </Suspense>
+      )
+    case 'page3':
+      const AsyncPage3 = AsyncComponent(()=>import('./components/Page3'))
+      return <AsyncPage3 onRouteChange={onRouteChange}/>
+  }
+    
 }
 
 export default App;
